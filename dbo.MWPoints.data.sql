8a3d2df7fb1f6a08c9d653867ec8c70e5ad1cb76
EngineerDDP <zzaddp@live.com>
2019-10-10 09:43:13 +0800

    Delete dbo.MWPoints.data.sql

--- dbo.MWPoints.data.sql
+++
@@ -1,4 +0,0 @@
-﻿Select * from dbo.MWPoints where PointName IN (Select PointName from dbo.ThresholdPoints where PointID = '67')
---Select * From dbo.VIEW_T_ZB_EA where INSTR_NO = 'C06-09'
---select * From dbo.VIEW_T_ZB_EA_AS where pname = '9'
---select * from dbo.View_T_ZB_MX_AS where pname = '67'
